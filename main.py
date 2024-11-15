558c4182ba26d1d1568c1f82437f0c40ad8d5d9b
unknown <D:\work\mail>
2024-11-15 17:21:52 +0300

    Добавлен мяч.

--- main.py
+++ main.py
@@ -19,19 +19,37 @@ class Player ():
         if keys[K_DOWN] and self.rect.y<565:
             self.rect.y+=8
             data['y']+=8
-        connection.send_data(data)
+        connection.send_data('ry'+str(data['y']))
+    def get_move(self,y):
+        self.rect.y=y
+class Ball ():
+    def __init__(self, x, y):
+        self.y=y
+        self.x=x
+        self.image=transform.scale(image.load('sources/istockphoto.png'),(70, 70))
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+    def get_move(self,x,y):
+        self.rect.x=x
+        self.rect.y=y
+class Reciever():
     def get_move(self):
         new_data=connection.recieve_data()
         new_data=new_data.decode()
-        new_data=new_data.replace('{','').replace('}','').replace("'", "").split(',')
-        x=new_data[0].split(':')[1]
-        y=new_data[1].split(':')[1]
-        self.rect.x=int(x)
-        self.rect.y=int(y)
+        new_data=new_data.split(':')
+        ball_x=int(new_data[1].split(',')[0][1:])
+        ball_y=int(new_data[1].split(',')[1][1:])
+        player_y=int(new_data[0][2:])
+        return ball_x,ball_y,player_y
 window=display.set_mode((1280,720))
 background=transform.scale(image.load('sources/0b113edf6db30d09a5dff8cca6bfc813.png'),(1280,720))
 rocketka_number_one=Player(38,285)
 rocketka_number_two=Player(1210,285)
+ball=Ball(605, 325)
+reciever = Reciever()
 igrovoi_cikl=True
 clock=time.Clock()
 display.set_caption('Игра в пинг-понг онлайн')
@@ -41,9 +59,13 @@ while igrovoi_cikl:
     rocketka_number_one.reset()
     rocketka_number_two.reset()
     rocketka_number_one.move()
-    rocketka_number_two.get_move()
+    ball.reset()
+    ball_x,ball_y,player_y=reciever.get_move()
+    rocketka_number_two.get_move(player_y)
+    ball.get_move(ball_x,ball_y)
     for i in event.get():
         if i.type==QUIT:
             igrovoi_cikl = False
+            connection.close_connection()
     clock.tick(60)
     display.update()
--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 import socket
 host='127.0.0.1'
 port=18083
+ball_cords={"x":605,'y':325}
+ball_speed=2
 with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
     s.bind((host,port))
     s.listen()
     connection1,address1=s.accept()
-    connection2,address2=s.accept()
     print('Соединение:', address1)
+    connection2,address2=s.accept()
     print('Соединение:', address2)
     while True:
+        ball_cords['x']+=ball_speed
+        ball_cords['y']+=ball_speed
         data1=connection1.recv(1024)
         data2=connection2.recv(1024)
         if not data1 and not data2:
             break
-        connection1.sendall(data2)
-        connection2.sendall(data1)
+        connection1.sendall(data2+f':x{ball_cords["x"]},y{ball_cords["y"]}'.encode())
+        print(data1+f':x{ball_cords["x"]},y{ball_cords["y"]}'.encode())
+        connection2.sendall(data1+f':x{ball_cords["x"]},y{ball_cords["y"]}'.encode())
